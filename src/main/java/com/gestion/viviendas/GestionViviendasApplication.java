144dee4e2e3a4a4303896c78661e2239e5ea4a95
Bren pc <brayanp.cabrera@gmail.com>
2024-09-08 14:40:47 -0500

    first commit

---
+++ src/main/java/com/gestion/viviendas/GestionViviendasApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.viviendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionViviendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionViviendasApplication.class, args);
+	}
+
+}
