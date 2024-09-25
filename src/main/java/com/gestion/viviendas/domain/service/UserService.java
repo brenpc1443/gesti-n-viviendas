31720ba54e8f57bf0b18d67b1d5b0c1bbf882714
Bren pc <brayanp.cabrera@gmail.com>
2024-09-25 00:06:59 -0500

    se implementaron los servicios

--- src/main/java/com/gestion/viviendas/domain/service/UserService.java
+++ src/main/java/com/gestion/viviendas/domain/service/UserService.java
@@ -1,11 +1,46 @@
 package com.gestion.viviendas.domain.service;
 
+import com.gestion.viviendas.domain.User;
 import com.gestion.viviendas.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
+
+    public List<User> getAll(){
+        return userRepository.getAll();
+    }
+
+    public Optional<User> getById(int userId){
+        return userRepository.getById(userId);
+    }
+
+    public Optional<List<User>> findByNombreOrApellido(String filtro){
+        return userRepository.findByNombreOrApellido(filtro);
+    }
+
+    public Optional<User> getByDni(String dni){
+        return userRepository.getByDni(dni);
+    }
+
+    Optional<List<User>> getByRol(String rol){
+        return userRepository.getByRol(rol);
+    }
+
+    public User save(User user){
+        return userRepository.save(user);
+    }
+    public boolean delete(int userId){
+        return getById(userId).map(user -> {
+            userRepository.delete(userId);
+            return true;
+        }).orElse(false);
+    }
 }
