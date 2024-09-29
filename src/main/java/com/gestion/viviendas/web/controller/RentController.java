89488eed92330ee05f95f2477eb937850a8ed2e0
Bren pc <brayanp.cabrera@gmail.com>
2024-09-29 12:20:46 -0500

    habilitación de CORS

--- src/main/java/com/gestion/viviendas/web/controller/RentController.java
+++ src/main/java/com/gestion/viviendas/web/controller/RentController.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/rents")
 public class RentController {
